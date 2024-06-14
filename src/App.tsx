0db7c07434c4cba1671d59cf2421c3102cd026f8
Túlio <tuliovictorp@hotmail.com>
2024-06-13 23:17:08 -0300

    feat: remove task

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,14 @@ import { ITask } from "./interfaces/Task";
 function App() {
   const [taskList, setTaskList] = useState<ITask[]>([]);
 
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter((task) => {
+        return task.id !== id;
+      })
+    );
+  };
+
   return (
     <div>
       <Header />
@@ -23,7 +31,7 @@ function App() {
         </div>
         <div>
           <h2>Suas tarefas:</h2>
-          <TaskList taskList={taskList} />
+          <TaskList taskList={taskList} handleDelete={deleteTask} />
         </div>
       </main>
       <Footer />
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,23 +1,27 @@
-import React from "react";
+import styles from "./TaskList.module.css";
 import { ITask } from "../interfaces/Task";
 
 interface Props {
   taskList: ITask[];
+  handleDelete(id: number): void;
 }
 
-const TaskList = ({ taskList }: Props) => {
+const TaskList = ({ taskList, handleDelete }: Props) => {
   return (
     <>
       {taskList.length > 0 ? (
         taskList.map((task) => (
-          <div key={task.id}>
-            <div>
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
               <h4>{task.taskName}</h4>
               <p>Dificuldade: {task.difficulty}</p>
             </div>
-            <div>
-              <i className='bi bi-pencil'></i>
-              <i className='bi bi-trash'></i>
+            <div className={styles.actions}>
+              <i className='bi bi-pencil' id={styles.edit_icon}></i>
+              <i
+                className='bi bi-trash'
+                id={styles.delete_icon}
+                onClick={() => handleDelete(task.id)}></i>
             </div>
           </div>
         ))
