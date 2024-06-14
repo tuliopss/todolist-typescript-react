d7440a1208c8177d48671a26d0a418b3a62fa293
Túlio <tuliovictorp@hotmail.com>
2024-06-13 22:51:14 -0300

    feat: show task list

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
         </div>
         <div>
           <h2>Suas tarefas:</h2>
-          <TaskList />
+          <TaskList taskList={taskList} />
         </div>
       </main>
       <Footer />
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { ITask } from "../interfaces/Task";
 
-type Props = {};
+interface Props {
+  taskList: ITask[];
+}
 
-const TaskList = (props: Props) => {
+const TaskList = ({ taskList }: Props) => {
   return (
-    <div>
-      <p>task 1</p>
-    </div>
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id}>
+            <div>
+              <h4>{task.taskName}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div>
+              <i className='bi bi-pencil'></i>
+              <i className='bi bi-trash'></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Sem tarefas cadastradas...</p>
+      )}
+    </>
   );
 };
 
